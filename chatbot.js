3bfff90b7c5fd0d6f759aa53e765f4be9d817357
Sam Agnew <sagnew@eden.rutgers.edu>
2016-04-14 19:39:26 -0400

    Implement basic Twitch Plays Pokemon for chat

--- chatbot.js
+++ chatbot.js
@@ -1,5 +1,7 @@
 // Do NOT include this line if you are using the browser version.
 var irc = require("tmi.js");
+var fs = require('fs');
+var gameboyButtons = ['a', 'b', 'left', 'right', 'up', 'down', 'start', 'select'];
 
 var options = {
     options: {
@@ -21,9 +23,17 @@ var client = new irc.client(options);
 // Connect the client to the server..
 client.connect();
 client.on('connected', function(channel, user, message, self) {
-  client.say('sagnewshreds', 'HELLO TWITCH WHAT UP');
+  client.say('sagnewshreds', 'THIS IS NOW TWITCH PLAYS POKEMON CHAT! HYPE!');
 });
 
 client.on("chat", function (channel, user, message, self) {
+  var button = message.toLowerCase();
+
   console.log(user.username + ': ' + message);
+  if(gameboyButtons.indexOf(button) >= 0) {
+    if(button === 'a' || button === 'b') {
+      button = button.toUpperCase();
+    }
+    fs.writeFileSync('button.txt', button, 'utf8');
+  }
 });
